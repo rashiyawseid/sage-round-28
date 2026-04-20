715753d755cac26da09107e46e7b77a6f93d84df
gelil 1 <galilazemedu@gmail.com>
2026-04-20 04:06:15 -0700

    operation

--- data tayp.cpp
+++ data tayp.cpp
@@ -3,6 +3,16 @@ using namespace std;
 int main(){
 	int age;
 	age=20;
-	cout<<age;
+	float height =1.7;
+	char grade ='A';
+	bool pass = false;
+	string name = "Abebe";
+	const double pi=3.14;
+	cout<<"Age:"<<age<<endl;
+	cout<<height<<endl;
+	cout<<grade<<endl;
+	cout<<pass<<endl;
+	cout<<name<<endl;
+	cout<<pi;
 	return 0;
 }
---
+++ opertion c++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<10+12<<endl;
+	cout<<10-12<<endl;
+	cout<<10*12<<endl;
+	cout<<10/5<<endl;
+	cout<<10%8<<endl;
+	int a=15;
+	int b=15;
+	int c=15,d=10;
+	cout<<"a+b=" <<a+b<<endl;
+	cout<<"a-b=" <<a-b<<endl;
+	return 0;
+}
