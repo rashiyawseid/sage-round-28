a8e50b7ef15a3a23fe9715535783445d7337214f
gelil 1 <galilazemedu@gmail.com>
2026-04-21 02:45:05 -0700

    faranite

---
+++ fahranite.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+	double c^0,f^0;
+	cout<<"enter the celicous: ";
+	cin>>c^0;
+	f^0=(c*1.8)+32;
+	cout<<"fahranite= "<<f^0<<endl;
+	return 0;
+	
+}
