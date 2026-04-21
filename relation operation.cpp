a95299c5ca32beb9d013fdabc2e9bccb2a7eea56
gelil 1 <galilazemedu@gmail.com>
2026-04-21 01:40:47 -0700

    relation operation

---
+++ relation operation.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a=50,b=50;
+	bool grater=a>b;
+	bool leasthan=a<b;
+	bool graterthanorequal=a>=b;
+	bool leasthanorequal=a<=b;
+	bool equal=a==b;
+	bool notequal=a!=b;
+	cout<<"a: " <<a<<" "<<"b: " <<b<<endl;
+	cout<<"graterthan:"<<grater<<endl;
+	cout<<"leasthan:"<<leasthan<<endl;
+	cout<<"graterthanorequal:"<<graterthanorequal<<endl;
+	cout<<"leasthanorequal:"<<leasthanorequal<<endl;
+	cout<<"equal:"<<equal<<endl;
+	cout<<"notequal:"<<notequal<<endl;
+	return 0;
+	
+}
