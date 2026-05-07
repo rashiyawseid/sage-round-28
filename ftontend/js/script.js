3b23feef73cb526327756d40cbfffc596318e558
rashiyawseid <rashiyawseid@gmail.com>
2026-05-07 13:58:23 +0300

    html advance

---
+++ ftontend/js/script.js
@@ -0,0 +1,27 @@
+function externalAlert(){
+    alert("external")
+}
+//variable
+let name="abebe";
+console.log(name)
+
+let age=20
+age=17
+console.log(age)
+
+const pi=3.14
+console.log(pi)
+
+let x=1, y=2
+console.log(x+y)
+
+let isactive=true
+console.log(isactive)
+
+let fruits=["banana","mango","papaya"]
+console.log(fruits)
+fruits[0]="apple"
+console.log(fruits)
+
+
+
