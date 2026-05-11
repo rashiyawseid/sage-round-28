5b408ffa24e16d3166ee7a175ce4454f43f6dea8
rashiyawseid <rashiyawseid@gmail.com>
2026-05-11 14:05:50 +0300

    css spacing manamn

--- ftontend/js/script.js
+++ ftontend/js/script.js
@@ -22,6 +22,62 @@ let fruits=["banana","mango","papaya"]
 console.log(fruits)
 fruits[0]="apple"
 console.log(fruits)
+let a=1 ,b='1'
+console.log(a==b)
+console.log(a===b)
 
+console.log(a!=b)
+console.log(a!==b)
+
+function sum(){
+    console.log(30+40)
+}
+
+sum()
+sum()
+
+function multiplication(){
+    console.log(x*y)
+}
+
+multiplication(30,40)
+multiplication(300,400)
+
+function avarage(x,y){
+    console.log((x+y)/2)
+}
+
+avarage(20,30)
+
+let myage=20
+
+if(myage>=18){
+     console.log("you are an adult")
+}
+else{
+    console.log("you are a minor")
+}
+
+let score =60
+
+if(score>=58){
+   console.log("you are passed")
+}
+else{
+    console.log("you are failed")
+}
+
+for(let i=3;i<0;i++){
+    console.log(i)
+}
+    let i=0
+
+    while(i<5){
+     console.log(i)   
+     i++
+}
+
+let title=document.getElementById('title')
+title.innerText="this is the the text from javascript !"
 
 
