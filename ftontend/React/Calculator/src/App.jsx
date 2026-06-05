c5d944de0f4e8a2d5be6d967c27eae70cb05c963
seada <seadaseamn718@gmail.com>
2026-06-05 12:20:03 +0300

    calculator

---
+++ ftontend/React/Calculator/src/App.jsx
@@ -0,0 +1,35 @@
+const App = ( =>{
+  returrn(
+    <div ClassName="calculator-container">
+      <div ClassName="calculator">
+        <div ClassName="display">0</div>
+
+        <div ClassName="buttons">
+          <button ClassNme="btn control">c</button>
+          <button ClassNme="btn control">Del</button>
+          <button ClassNme="btn control">%</button>
+          <button ClassNme="btn control">/</button>
+
+          <button ClassNme="btn">7</button>
+          <button ClassNme="btn">8</button>
+          <button ClassNme="btn">9</button>
+          <button ClassNme="btn operator">*</button>
+
+          <button ClassNme="btn">4</button>
+          <button ClassNme="btn">5</button>
+          <button ClassNme="btn">6</button>
+          <button ClassNme="btn operator">+</button>
+
+          <button ClassNme="btn">1</button>
+          <button ClassNme="btn">2</button>
+          <button ClassNme="btn">3</button>
+          <button ClassNme="btn operator">-</button>
+
+          <button ClassNme="btn zero">0</button>
+          <button ClassNme="btn">.</button>
+          <button ClassNme="btn equals">=</button>
+        </div>
+      </div>
+    </div>
+  )
+})
